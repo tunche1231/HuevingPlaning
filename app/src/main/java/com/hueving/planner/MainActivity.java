60066852922f81e7a9349ee2817be049cc4e968f
Diego <diegotz20@hotmail.com>
2019-10-24 21:09:40 -0500

    Agregado hueving :D

--- app/src/main/java/com/hueving/planner/MainActivity.java
+++ app/src/main/java/com/hueving/planner/MainActivity.java
@@ -3,6 +3,8 @@ package com.hueving.planner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+//:D HuevingPlanning
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
